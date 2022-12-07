a3e46ca78745789e5a3c596cd5757f45f947bade
madtrx <marlin.makori@gmail.com>
2022-12-07 09:59:10 -0700

    new text to speech

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,51 +1,50 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM fully loaded and parsed');
+  //declaring a new instance of JS speech API
+  let speech = new SpeechSynthesisUtterance();
+  
+  //assigning a language
+  speech.lang = "en";
+  
+  //creating an empty array that will contain voices
+  let voice = []
+  
+  //load voices asynchronously
+  window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[51];
+  }
 
-let speech = new SpeechSynthesisUtterance();
-speech.lang = "en";
-
-let voices = [];
-
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0];
-  let voiceSelect = document.querySelector("#voices");
-  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
-};
-
-document.querySelector("#rate").addEventListener("input", () => {
-  const rate = document.querySelector("#rate").value;
-  speech.rate = rate;
-  document.querySelector("#rate-label").innerHTML = rate;
-});
-
-document.querySelector("#volume").addEventListener("input", () => {
-  const volume = document.querySelector("#volume").value;
-  speech.volume = volume;
-  document.querySelector("#volume-label").innerHTML = volume;
-});
-
-document.querySelector("#pitch").addEventListener("input", () => {
-  const pitch = document.querySelector("#pitch").value;
-  speech.pitch = pitch;
-  document.querySelector("#pitch-label").innerHTML = pitch;
-});
-
-document.querySelector("#voices").addEventListener("change", () => {
-  speech.voice = voices[document.querySelector("#voices").value];
-});
-
-document.querySelector("#start").addEventListener("click", () => {
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
-});
-
-document.querySelector("#pause").addEventListener("click", () => {
-  window.speechSynthesis.pause();
-});
-
-document.querySelector("#resume").addEventListener("click", () => {
-  window.speechSynthesis.resume();
-});
-
-document.querySelector("#cancel").addEventListener("click", () => {
-  window.speechSynthesis.cancel();
+  speech.volume = 1
+  let captions = document.querySelectorAll("#captions > div")
+  let i = 1
+  const speechCompleted = () => {
+    speech.text = captions[i].innerText
+    // console.log(speech.text, i)
+    window.speechSynthesis.speak(speech);
+    i++
+    if (i > captions.length -1) {
+      clearInterval(readCaptions)
+    }
+  }
+ 
+  document.querySelector("#start").addEventListener("click", () => {
+    //when we press starrt its wait 6 seconda before ==> this delay can be annoying for a user
+    //the set interval doesnt stop ==> clear set interval
+    speech.text = captions[0].innerText
+    window.speechSynthesis.speak(speech);
+    const readCaptions = setInterval(speechCompleted, 6000) //just enuogh to fully read the whole prompt
+  });
+  
+  document.querySelector("#pause").addEventListener("click", () => {
+    window.speechSynthesis.pause();
+  });
+  
+  document.querySelector("#resume").addEventListener("click", () => {
+    window.speechSynthesis.resume();
+  });
+  
+  document.querySelector("#cancel").addEventListener("click", () => {
+    window.speechSynthesis.cancel();
+  });
 });
